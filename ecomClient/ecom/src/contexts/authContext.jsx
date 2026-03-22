a6d104018c14d2441279117e6abaf76db60fa001
Rohan  Nagargoje <rohan.nagargoje@rheal.com>
2026-03-22 21:14:10 +0530

    Fix import path for authApi in AuthContext

--- ecomClient/ecom/src/contexts/authContext.jsx
+++ ecomClient/ecom/src/contexts/authContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext,useContext,useState,useEffect } from "react";
 const AuthContext = createContext();
-import {loginApi,registerApi} from "../api/AuthApi";
+import {loginApi,registerApi} from "../api/authApi";
 import { jwtDecode } from "jwt-decode";
 
 
